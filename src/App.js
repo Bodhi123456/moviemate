c604ae396e29c530a0ed8f8cfa8607559aeffa4b
Bodhi123456 <120386476+Bodhi123456@users.noreply.github.com>
2024-09-10 23:44:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@
 import React from 'react';
 import './App.css';
 import MovieSearch from './component/MovieSearch';
-import {Header} from "./component/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
       <MovieSearch />
     </div>
   );
